afab62ce8420420d3ef193340d12f81b1c96d719
mitchgillin <mitchtgillin@gmail.com>
2019-12-05 08:36:28 -0500

    Working part 1

--- Day3/Day3.js
+++ Day3/Day3.js
@@ -7,47 +7,82 @@ const wires = fs
 
 const calcManhattanDistance = (p, q) => {
   return Math.abs(p[0] - q[0]) + Math.abs(p[1] - q[1]);
-};
-
-const walkPath = (command, pos, path) => {};
+}
 
 const generatePathArray = wireString => {
   const wire = wireString.split(",");
   let pos = { x: 0, y: 0 };
   let path = [];
+  let steps = 0;
   for (let i = 0; i < wire.length; i++) {
     const distance = parseInt(wire[i].substring(1));
     switch (wire[i][0]) {
       case "R":
-        for (let x = pos.x; x < pos.x + distance; x++) {
-          path.push([x, pos.y]);
+        for (let x = pos.x; x <= pos.x + distance; x++) {
+          path.push([x, pos.y, steps++]);
         }
-        break;
-      case "U":
-        for (let y = pos.y; y < pos.y + distance; y++) {
-          path.push([pos.x, y]);
-        }
-        break;
-      case "D":
-        for (let y = pos.y; y > pos.y - distance; y--) {
-          path.push([pos.x, y]);
-        }
+        pos.x = path[path.length-1][0];
         break;
       case "L":
-        for (let x = pos.x; x > pos.x - distance; x--) {
-          path.push([x, pos.y]);
+        for (let x = pos.x; x >= pos.x - distance; x--) {
+          path.push([x, pos.y, steps++]);
         }
+        pos.x = path[path.length-1][0]; 
         break;
+      case "U":
+        for (let y = pos.y; y <= pos.y + distance; y++) {
+          path.push([pos.x, y, steps++]);
+        }
+        pos.y= path[path.length-1][1];
+        break;
+      case "D":
+        for (let y = pos.y; y >= pos.y - distance; y--) {
+          path.push([pos.x, y, steps++]);
+        }
+        pos.y= path[path.length-1][1]; 
+        break;
+      
     }
   }
   return path;
 };
 
+const compareArrays = (path1, path2) => {
+  let commonPoints = [];
+  path1.forEach((point1 => {
+    path2.forEach((point2)=>{
+      if(point1[0] == point2[0] && point1[1] == point2[1]){
+        console.log(point1[2] + point2[2])
+        commonPoints.push(point1)
+      }
+    })
+  }))
+ 
+  return commonPoints;
+}
+ 
+
+
 const Day3A = () => {
   const path1 = generatePathArray(wires[0]);
   const path2 = generatePathArray(wires[1]);
 
-  return generatePathArray(wires[0]);
+  const commonPoints = compareArrays(path1, path2);
+
+  const distances = commonPoints.map((point)=> {
+    return calcManhattanDistance([0,0], point)
+  });
+
+  return Math.min(...distances.filter(dist=> dist !=0)); 
 };
 
-export { Day3A };
+const Day3B = () => {
+  const path1 = generatePathArray(wires[0]);
+  const path2 = generatePathArray(wires[1]);
+
+  const commonPoints = compareArrays(path1, path2);
+
+  return commonPoints;
+}
+
+export { Day3A, Day3B };
